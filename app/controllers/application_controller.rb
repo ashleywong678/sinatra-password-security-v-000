3994797e4c1371b8b5d7e314c941bbc7d869ac52
Ashley Wong <ashwong04@gmail.com>
2019-03-04 22:44:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,10 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		user = User.new(:username => params[:username], :password => params[:password])
-  	if user.save
-      redirect "/login"
-    else
+  	if !user.save || user
       redirect "/failure"
+    else
+      redirect "/login"
     end
 	end
 
